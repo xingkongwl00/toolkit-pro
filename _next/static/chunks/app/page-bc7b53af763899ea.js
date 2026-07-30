53485cb0089cb1daa8a432e1b85d723b88259b98
xingkongwl00 <1035342329@qq.com>
2026-07-30 14:43:24 +0800

    Fix asset paths with basePath for GitHub Pages

---
+++ _next/static/chunks/app/page-bc7b53af763899ea.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{3156:function(e,n,s){Promise.resolve().then(s.bind(s,2228)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[717,971,117,744],function(){return e(e.s=3156)}),_N_E=e.O()}]);
